04e0f3f22088d4bbde82938d5adff73ca269b030
Daele-art <dalebout.wesley@gmail.com>
2025-09-24 14:02:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 
 export default defineConfig({
-  base: '/', // root because username-pages lives at /
   plugins: [react()],
-  resolve: {
-    alias: { '@': resolve(process.cwd(), './src') },
-    extensions: ['.mjs','.js','.jsx','.ts','.tsx','.json']
-  },
-  optimizeDeps: { esbuildOptions: { loader: { '.js': 'jsx' } } }
+  base: '/',           // belangrijk voor custom (root) domein
+  build: { outDir: 'dist' }
 })
